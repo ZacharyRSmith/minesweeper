79573a25f8f50048b4759d1a29fd4b2cdb18e139
ZacharyRSmith <ZacharySmith4989@gmail.com>
2014-12-24 10:36:50 +0000

    Outline constructors and prototypes.

--- minesweeper.js
+++ minesweeper.js
@@ -7,3 +7,47 @@
 // numFlags = numMines
 // Flags/?'s
 // squares next to mine display numMines touching.
+
+// OBJECTS:
+// Game
+//   init:
+//    genGrid(size)
+//    placeMines(num)
+//    setNumTouchingMines
+//
+//    renderGrid:function
+//
+//   numSquaresDiscovered
+//     if grid.length ** 2 - numMines { victory = true; }
+//   checkVictory
+//   setVictory
+
+function Square() {
+  this.isDiscovered = false;
+  this.hasMine = false;
+  this.view = " "; //   view (Mine, numMines, flag, question, blank)
+}
+
+Square.prototype = {
+  constructor: Square,
+  discoverAdjSquares:function() {
+    look at squares adjacent on grid:
+    setToDiscovered(adjSquare);
+  },
+  getNumTouchingMines:function() {
+    // look at adjacent squares on grid. return count that hasMine
+  },
+  getView:function() {},
+  setToDiscovered:function() {
+    if (hasMine) {
+
+    } else if (getNumTouchingMines > 0) {
+
+    } else { // is a "blank square"
+      discoverAdjSquares
+    }
+  },
+  setView:function() {},
+  setViewToFlag:function() {},
+  setViewToQuestion:function() {}
+}
