5662f7bb5a675519055f4c57dcf3683175f205d5
ZacharyRSmith <ZacharySmith4989@gmail.com>
2014-12-24 09:24:50 +0000

    Add comments of requirements to minesweeper.js.

---
+++ minesweeper.js
@@ -0,0 +1,9 @@
+// Start with a 9x9 board with 10 mines.
+
+// EVENTS:
+// VICTORY -- All squares (-mines) are showing.
+// LOSS -- Click on mine.
+
+// numFlags = numMines
+// Flags/?'s
+// squares next to mine display numMines touching.
